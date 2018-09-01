bd84c996d26fd95afd3034a0e3842cd71037f42c
luizamazo <luizamazo@hotmail.com>
2018-09-01 10:35:37 -0300

    side menu

--- frontend/src/app/dashboard/sidemenu.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default props => (
-    <div class="col-md-3 left_col">
-        <div class="left_col scroll-view">
-        
-        </div>
-    </div>
-)
---
+++ frontend/src/app/dashboard/sm-title.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default props => (
+
+    <div class="profile">
+            <div class="profile_pic">
+                <img src="images/img.jpg" alt="..." class="img-circle profile_img"/>
+            </div>
+            <div class="profile_info">
+                <span>Welcome,</span>
+                <h2>Anthony Mutisya</h2>
+            </div>
+    </div>
+
+
+)
