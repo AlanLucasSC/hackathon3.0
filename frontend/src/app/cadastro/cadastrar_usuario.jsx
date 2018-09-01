c17f32b09034f70f1e47a9a74d929b750734b136
luizamazo <luizamazo@hotmail.com>
2018-09-01 17:49:18 -0300

    layout arrumado

--- frontend/src/app/cadastro/cadastrar_usuario.jsx
+++ frontend/src/app/cadastro/cadastrar_usuario.jsx
@@ -1,44 +1,52 @@
 import React from 'react'
 
 export default props => (
+   
    <div className="container"> 
      <div className="row">
-        <div className="col-xs-offset-6">
-            <div className="card border">
+        <div className="col-md-6 offset-md-3">
+            <div className="card border" id="cadastroUsuario">
                 <div className="card-body">
-                    <h5 class="card-title">Cadastre-se</h5>
+                    <h3 class="card-title">Cadastre-se</h3>
+                      <h6 className="card-subtitle mb2 text-muted">(*) Campos Obrigatórios</h6>
                         <form encType="multipart/form-data">
                             <div className="form-group">
-                                <label For="name">Nome Completo</label>
-                                <input className="form-control" id="name" placeholder="Nome Completo" 
-                                value={props.name} onChange={props.handleChangeName}/>
+                                <strong><label For="nome">Nome Completo</label></strong>
+                                <input className="form-control" id="nome" placeholder="Nome Completo" 
+                                value={props.nome} onChange={props.handleChangeNome}/>
                             </div>
 
                             <div className="form-group">
-                                <label For="email">Email</label>
+                            <strong><label For="email">Email</label></strong>
                                 <input className="form-control" id="email" placeholder="Email" 
                                 value={props.email} onChange={props.handleChangeEmail}/>
                             </div>
 
                             <div className="form-group">
-                                <label For="cpf">CPF</label>
+                            <strong><label For="cpf">CPF</label></strong>
                                 <input className="form-control" id="cpf" placeholder="CPF" 
                                 value={props.cpf} onChange={props.handleChangeCpf}/>
                             </div>
 
                             <div className="form-group">
-                                <label For="rg">RG</label>
+                            <strong> <label For="rg">RG</label></strong>
                                 <input className="form-control" id="rg" placeholder="RG" 
                                 value={props.rg} onChange={props.handleChangeRg}/>
                             </div>
 
                             <div className="form-group">
-                                <label For="password">Senha</label>
+                            <strong> <label For="cargo">Cargo</label></strong>
+                                <input className="form-control" id="cargo" placeholder="Cargo" 
+                                value={props.cargo} onChange={props.handleChangeCargo}/>
+                            </div>
+
+                            <div className="form-group">
+                            <strong><label For="password">Senha</label></strong>
                                 <input className="form-control" id="password" placeholder="Senha" 
                                 value={props.password} onChange={props.handleChangePassword}/>
                             </div>
 
-                            <button type="submit" class="btn btn-primary btn-sm">Cadastrar</button>
+                            <button type="submit" class="btn btn-primary btn-md">Cadastrar</button>
                         </form>
                 </div>
             </div>
