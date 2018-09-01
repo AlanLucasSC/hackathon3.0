a8ffbdee0b837430208049fa5273e7497acae876
krdg10 <krdg10@live.com>
2018-09-01 13:10:00 -0500

    logout modal e main footer no dashboard

---
+++ frontend/src/app/dashboard/main_footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default props => (
+    <footer className="sticky-footer">
+          <div className="container my-auto">
+            <div className="copyright text-center my-auto">
+              <span>Copyright © Your Website 2018</span>
+            </div>
+          </div>
+    </footer>
+)
