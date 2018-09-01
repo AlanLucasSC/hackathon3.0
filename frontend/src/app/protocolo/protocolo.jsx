e7b33454fde47dfdd8b9eee5ea43779350f321b4
krdg10 <krdg10@live.com>
2018-09-01 12:29:42 -0500

    jsx do processo e do protocolo

---
+++ frontend/src/app/protocolo/protocolo.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+
+export default class Protocolo extends Component {
+    constructor(props){
+        super(props)
+        this.state = {}
+
+        this.refresh()
+       
+    }
+
+  
+   
+
+    render() {
+        return (
+            <div className='protocolo'>
+               
+              
+               
+
+              
+            </div>
+        )
+    }
+}
