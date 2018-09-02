f8c68508500939ceb6a43e6992017390e642ced1
AlanLucasSC <AlanLucasSC2011@gmail.com>
2018-09-02 09:52:18 -0300

    Upload de arquivo realizado no backend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -6,6 +6,7 @@ const express = require('express')
 const server = express()
 const allowCors = require('./cors')
 const fs = require('fs')
+const request = require("request");
 
 //Upload file
 const multer  = require('multer')
@@ -33,17 +34,52 @@ server.listen(port, function() {
 })
 
 server.post('/file', upload.single('image'), function (req, res, next) {
+  /**
+   * Upload na api da az
+   */
+  var options = { 
+    method: 'POST',
+    url: 'http://virtus.azi.com.br/virtus-rest/v1/arquivos',
+    headers: 
+    { 
+      protocolodoc: req.body.protocolodoc,
+      chavecategoria: req.body.chavecategoria,
+      numeroprocesso: req.body.numeroprocesso,
+      mimetype: req.body.mimetype,
+      nome: req.body.nome
+    },
+    formData: { 
+      '': { 
+          value: fs.createReadStream(req.file.path),
+          options: { 
+              filename: 'testePDF.pdf', 
+              contentType: null 
+          } 
+      } 
+    } 
+  }
+
+  request(options, function (error, response, body) {
+    if (error) {
+      res.json({
+        err: 'Houve algum erro na hora de enviar o documento'
+      })
+    };
+
+    console.log(body);
+  })
 
 	/**
    * Responder o cliente
    */
 	res.json({
-    image: image._id+'.'+ext
+    image: req.file.path
   })
 
 })
 
 //Deixar acessar a pasta public
 server.use('/public', express.static('public/'))
+server.use('/img', express.static('img/'))
 
 module.exports = server
