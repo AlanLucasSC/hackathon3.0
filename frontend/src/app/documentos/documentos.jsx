54ea3a38b43cad331e606af7855ed514e9821fba
krdg10 <krdg10@live.com>
2018-09-02 11:35:41 -0500

    documentos form  e algo na instituicao

--- frontend/src/app/documentos/documentos.jsx
+++ frontend/src/app/documentos/documentos.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import {  Link } from 'react-router-dom'
 import Form from './form'
-
+import Layout from '../layout/layout'
 const URL = 'http://virtus.azi.com.br/virtus-rest/v1/categorias'
 
 
@@ -13,7 +13,7 @@ export default class ListarDocumentos extends Component {
     
     constructor(props){
         super(props)
-        this.state = { list: [] }
+        this.state = { teste:'', list: [] }
        
 
         this.refresh()
@@ -22,19 +22,20 @@ export default class ListarDocumentos extends Component {
 
     refresh(){
         axios.get(`${URL}?sort=-createdAt`)
-            .then(resp => this.setState({...this.state, list: resp.data})) 
+            .then(resp => this.setState({...this.state, teste: '', list: resp.data})) 
        
     }
    
    
 
     render() {
+      
         return (
             <div className='Documentos'> 
-                <Form
-                    list = {this.state.list}
-                   
-                />
+            
+                <Layout>
+                    <Form  />
+                </Layout> 
              
            
             </div>
